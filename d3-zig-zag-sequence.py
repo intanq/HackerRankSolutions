ee92d76378b3e4c65488f8863835952aabbd3930
Intan Maharani <67723730+npim@users.noreply.github.com>
2022-02-18 10:50:57 +0800

    Update d3-zig-zag-sequence.py

--- d3-zig-zag-sequence.py
+++ d3-zig-zag-sequence.py
@@ -1,4 +1,9 @@
 def findZigZagSequence(a, n):
+    """
+    Given an array of n distinct integers, transform the array into a zig zag sequence by permuting the array elements. 
+    A sequence will be called a zig zag sequence if the first  k elements in the sequence are in increasing order and the last k elements are in decreasing order, 
+    where k = (n+1)/2. You need to find the lexicographically smallest zig zag sequence of the given array.
+    """
     a.sort()
     mid = int((n + 1)/2)-1
     a[mid], a[n-1] = a[n-1], a[mid]
