32d8da2fd0251262751d3f202b89c655a7952ea9
Intan Maharani <67723730+npim@users.noreply.github.com>
2022-02-18 10:45:59 +0800

    Create d2-counting-sort-1.py

---
+++ d2-counting-sort-1.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    int_array = [0] * 100
+    for number in arr:
+        int_array[number] += 1
+    return int_array
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
